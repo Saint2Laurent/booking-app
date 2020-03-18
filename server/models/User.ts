33c3885ace68824a479df476a57641a2c0e01a7e
iamtheef <eftihis.riise@gmail.com>
2020-03-18 15:04:15 +0200

    modeling

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,6 +1,8 @@
 import { prop, Typegoose, ModelType, InstanceType } from "typegoose";
 
+import * as validate from "../../common/validators";
+
 export class User extends Typegoose {
-  @prop()
-  name: String;
+  @prop({ validate: fullname => validate.isFullNameValid(fullname).isValid })
+  fullname: String;
 }
